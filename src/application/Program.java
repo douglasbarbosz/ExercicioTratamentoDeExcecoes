4d221009d1011654388e15f20affffd7002fb791
Douglas Barbosa <douglassbarbosaap@gmail.com>
2023-05-10 11:24:34 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,22 +13,25 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		
+		System.out.println("Enter account data: ");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double initialBalance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amountWithdraw = sc.nextDouble();
+		
 		try {
-			System.out.println("Enter account data: ");
-			System.out.print("Number: ");
-			int number = sc.nextInt();
-			System.out.print("Holder: ");
-			sc.nextLine();
-			String holder = sc.nextLine();
-			System.out.print("Initial balance: ");
-			double initialBalance = sc.nextDouble();
-			System.out.print("Withdraw limit: ");
-			double withdrawLimit = sc.nextDouble();
-			System.out.println();
-			System.out.print("Enter amount for withdraw: ");
-			double amountWithdraw = sc.nextDouble();
-			
-			Account acc = new Account(number, holder, initialBalance, withdrawLimit);
 			acc.withdraw(amountWithdraw);
 			System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
 		}
